58b65b1fda32bdee4bc696957f443644acc93575
nikolasbrandao <nikolasbrandao@gmail.com>
2017-09-17 20:01:23 -0300

    Começo da logica da escolha da escolha do aluno apos a escolha de ocorrencia

--- app/src/main/java/br/edu/ifma/labmov/spotted/OcorrenciasActivity.java
+++ app/src/main/java/br/edu/ifma/labmov/spotted/OcorrenciasActivity.java
@@ -3,6 +3,8 @@ package br.edu.ifma.labmov.spotted;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ import br.edu.ifma.labmov.spotted.R;
 import br.edu.ifma.labmov.spotted.dao.InfracaoDao;
 import br.edu.ifma.labmov.spotted.model.Infracao;
 
+
 /**
  * Created by Cara1 on 16/09/2017.
  */
@@ -20,29 +23,32 @@ import br.edu.ifma.labmov.spotted.model.Infracao;
 public class OcorrenciasActivity extends AppCompatActivity {
 
     private InfracaoDao infracaoDao;
-
+    private   ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ocorrencia);
-        ListView lista = (ListView) findViewById(R.id.listocorrencias);
+        lista = (ListView) findViewById(R.id.listocorrencias);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         ArrayList<Infracao> infr;
         infracaoDao = new InfracaoDao(this);
-
         infr = (ArrayList<Infracao>) infracaoDao.findAll();
-        /*
-        Infracao infra = new Infracao();
-        infra.setId(123);
-        infra.setDescricao("Usando bermuda");
-        infra.setClassificacao("Infracao");
-        infra.setGravidade(5);
-        infr.add(infra);
-        */
 
         ListaOcorrencia adapterocorrencia = new ListaOcorrencia(infr,this);
         lista.setAdapter(adapterocorrencia);
 
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int posicao, long id) {
+                Infracao infracao = (Infracao) lista.getItemAtPosition(posicao);
+                //Avançar com a informação do id da infração para a tela de listagem de alunos
+            }
+        });
 
     }
 }
