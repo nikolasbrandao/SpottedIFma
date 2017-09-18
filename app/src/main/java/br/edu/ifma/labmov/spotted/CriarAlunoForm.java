0a500a974adffd5ddb887338cf2fce39f3a34394
gtgandrade <gtgandrade@gmail.com>
2017-09-18 15:45:42 -0300

    Adição do cadastro de aluno com geração de qr code;

--- app/src/main/java/br/edu/ifma/labmov/spotted/CriarAlunoForm.java
+++ app/src/main/java/br/edu/ifma/labmov/spotted/CriarAlunoForm.java
@@ -1,11 +1,24 @@
 package br.edu.ifma.labmov.spotted;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.Hashtable;
 
 import br.edu.ifma.labmov.spotted.dao.AlunoDao;
 import br.edu.ifma.labmov.spotted.model.Aluno;
@@ -14,8 +27,8 @@ public class CriarAlunoForm extends AppCompatActivity {
     Aluno aluno;
     AlunoDao daoAluno;
 
-    EditText edtNomeAluno;
-    EditText edtCodigoAluno;
+    static EditText edtNomeAluno;
+    static EditText edtCodigoAluno;
     EditText edtTurma;
     EditText edtAno;
 
@@ -51,6 +64,12 @@ public class CriarAlunoForm extends AppCompatActivity {
 
         Toast.makeText(this,torradeira,Toast.LENGTH_SHORT).show();
         daoAluno.close();
-
+        navegaActivityQRCode();
     }
+
+    public void navegaActivityQRCode(){
+        Intent navegaQRCode = new Intent(this, GerarQRCodeAluno.class);
+        startActivity(navegaQRCode);
+    }
+
 }
---
+++ app/src/main/java/br/edu/ifma/labmov/spotted/GerarQRCodeAluno.java
@@ -0,0 +1,50 @@
+package br.edu.ifma.labmov.spotted;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class GerarQRCodeAluno extends AppCompatActivity {
+
+    TextView txtAlunoCadastrado;
+    ImageView imgQRCodeAluno;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerar_qrcode_aluno);
+        txtAlunoCadastrado = (TextView) findViewById(R.id.txtAlunoCadastrado);
+        imgQRCodeAluno = (ImageView) findViewById(R.id.imgQRCodeAluno);
+        gerarQRCode();
+    }
+
+    public void gerarQRCode(){
+        String codigo = CriarAlunoForm.edtCodigoAluno.getText().toString();
+        String aluno = CriarAlunoForm.edtNomeAluno.getText().toString();
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(codigo, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            imgQRCodeAluno.setImageBitmap(bmp);
+            txtAlunoCadastrado.setText(aluno);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+}
